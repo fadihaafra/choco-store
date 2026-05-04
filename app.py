d15c218c8c3455375afc1c696740c9209561b7bb
fadihaafra <fadihafra@gmail.com>
2026-05-04 13:33:40 +0530

    update port for deployment

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def order():
     return render_template('order.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
